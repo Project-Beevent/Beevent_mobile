12f2a4f36fa25c6c6d8ea98773aefb87778de617
SelinDurmus16 <durmusselin8@gmail.com>
2024-01-02 17:48:48 +0300

    updating design process,AddRequestPage UI v.4

--- lib/View/AddRequestPage.dart
+++ lib/View/AddRequestPage.dart
@@ -19,186 +19,195 @@ class _AddRequestPageState extends State<AddRequestPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Request'),
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.deepOrangeAccent,
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Request Information',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.orange.shade100, Colors.deepOrange.shade100],
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Request Information',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Title',
+                SizedBox(height: 20),
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Title',
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 128.0),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(5.0),
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 128.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: DropdownButton<String>(
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                    items: <String>[
+                      'A+',
+                      'A-',
+                      'AB+',
+                      'AB-',
+                      'B+',
+                      'B-',
+                      'O+',
+                      'O-'
+                    ].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedBloodType = newValue;
+                      });
+                    },
+                    value: selectedBloodType,
+                    hint: Text('Blood Type'),
+                  ),
                 ),
-                child: DropdownButton<String>(
+                SizedBox(height: 20),
+                Text(
+                  'Description',
                   style: TextStyle(
                     fontSize: 18,
-                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
-                  items: <String>[
-                    'A+',
-                    'A-',
-                    'AB+',
-                    'AB-',
-                    'B+',
-                    'B-',
-                    'O+',
-                    'O-'
-                  ].map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedBloodType = newValue;
-                    });
-                  },
-                  value: selectedBloodType,
-                  hint: Text('Blood Type'),
                 ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Description',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+                SizedBox(height: 10),
+                TextField(
+                  controller: descriptionController,
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                  maxLines: 5, // Maksimum satır sayısı
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Description',
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: descriptionController,
-                style: TextStyle(
-                  fontSize: 15,
-                ),
-                maxLines: 5, // Maksimum satır sayısı
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Description',
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Hospital and Location',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 132.0),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                child: DropdownButton<String>(
+                SizedBox(height: 20),
+                Text(
+                  'Hospital and Location',
                   style: TextStyle(
                     fontSize: 18,
-                    color: Colors.black,
-                  ),
-                  items: <String>[
-                    'Ankara',
-                    'İstanbul',
-                    'İzmir',
-                    'Adana',
-                    'Adıyaman',
-                  ].map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedCity = newValue;
-                    });
-                  },
-                  value: selectedCity,
-                  hint: Text('City'),
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 90.0),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                child: DropdownButton<String>(
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black,
-                  ),
-                  items: <String>[
-                    'Florence Nightingale',
-                    'Acıbadem',
-                    'Memorial',
-                    'Medicana',
-                    'Anadolu',
-                  ].map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      selectedHospital = newValue;
-                    });
-                  },
-                  value: selectedHospital,
-                  hint: Text('Hospital'),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('Create Request'),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(380, 50),
-                  primary: Colors.indigo,
-                  onPrimary: Colors.white,
-                  shape: BeveledRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: _clearFields,
-                child: Text('Clear'),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(380, 50),
-                  primary: Colors.red,
-                  onPrimary: Colors.white,
-                  shape: BeveledRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 132.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: DropdownButton<String>(
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                    items: <String>[
+                      'Ankara',
+                      'İstanbul',
+                      'İzmir',
+                      'Adana',
+                      'Adıyaman',
+                    ].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedCity = newValue;
+                      });
+                    },
+                    value: selectedCity,
+                    hint: Text('City'),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 90.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: DropdownButton<String>(
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                    items: <String>[
+                      'Florence Nightingale',
+                      'Acıbadem',
+                      'Memorial',
+                      'Medicana',
+                      'Anadolu',
+                    ].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedHospital = newValue;
+                      });
+                    },
+                    value: selectedHospital,
+                    hint: Text('Hospital'),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Create Request'),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(380, 50),
+                    backgroundColor: Colors.amber,
+                    onPrimary: Colors.white,
+                    shape: BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: _clearFields,
+                  child: Text('Clear'),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(380, 50),
+                    primary: Colors.red,
+                    onPrimary: Colors.white,
+                    shape: BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
